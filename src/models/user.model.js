8a201fbc25e62a257622950fd76ba27f234b6335
Mukul Rajput <mukulrajput1430@gmail.com>
2025-10-08 02:33:39 +0530

    add access and refresh token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -57,7 +57,7 @@ userSchema.pre("save", async function (next){
 
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password) 
-}
+} 
 
 userSchema.methods.generateAccessToken = function () {
    return jwt.sign({
@@ -75,7 +75,7 @@ userSchema.methods.generateAccessToken = function () {
 
 userSchema.methods.generaterefreshToken = function () {
        return jwt.sign({
-        _id: this._id,
+        _id: this._id,      
    },
     process.env.REFRESH_TOKEN_SECRET,
     {
